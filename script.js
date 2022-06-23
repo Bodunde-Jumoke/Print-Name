24066510609b92696cf593b17d632a9be991e346
Bodunde-Jumoke <105549580+Bodunde-Jumoke@users.noreply.github.com>
2022-06-23 14:02:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let userName="Stacy";
+let myHeight="23 metres";
+let myCountry="Nigeria";
+
+alert("My name is" + " "+ userName +"\n" +"My height is"+" "+ myHeight +"\n" + "My Country is"+ " " + myCountry);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
